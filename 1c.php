bb1aef6f9ea4af0c68f56ba9541b11ada8e052c9
Nayara Christony Paul Barriga <92663332+SolidNoob502@users.noreply.github.com>
2023-02-23 11:29:59 +0700

    Update 1c.php

--- 1c.php
+++ 1c.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Square Stairs</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container"></div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Square Stairs</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container"></div>
     
-        <div class="flex-container-a"></div>
+            <div class="flex-container-a"></div>
 
-            <div class="flex-box-a">1</div>
+                <div class="flex-box-a">1</div>
         
-        <div class="flex-container-b"></div>
+            <div class="flex-container-b"></div>
 
-            <div class="flex-box-b">2</div>
-            <div class="flex-box-b">2</div>
+                <div class="flex-box-b">2</div>
+                <div class="flex-box-b">2</div>
 
-        <div class="flex-container-c"></div>
+            <div class="flex-container-c"></div>
         
-            <div class="flex-box-c">3</div>
-            <div class="flex-box-c">3</div>
-            <div class="flex-box-c">3</div>
+                <div class="flex-box-c">3</div>
+                <div class="flex-box-c">3</div>
+                <div class="flex-box-c">3</div>
 
-</body>
+    </body>
 </html>
