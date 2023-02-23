5d5136e76d7ba1e02a97741669451f2467201e7f
Nayara Christony Paul Barriga <92663332+SolidNoob502@users.noreply.github.com>
2023-02-23 11:28:07 +0700

    Add files via upload

---
+++ 1b.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mathematical Operations</title>
+</head>
+<body>
+    <p>
+
+        <?php 
+        
+        $number = 22;
+        
+        echo "Aku adalah angka $number<br>";
+        echo "Jika aku dikali 5, maka sekarang aku menjadi " . ($number * 5) . "<br>";
+        echo "Jika aku dibagi 2, maka sekarang aku menjadi " . ($number / 2) . "<br>";
+        echo "Jika aku ditambah 75, maka sekarang aku menjadi " . ($number + 75) . "<br>";
+        echo "Jika aku dikurang 20, maka sekarang aku menjadi " . ($number - 20) . "<br>";
+        
+        ?>
+
+    </p>
+</body>
+</html>
